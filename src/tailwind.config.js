dbbe7ebc95799e6fd9f6dbac143280dc049bd32b
Stefi <stefi.manci1@gmail.com>
2025-08-04 15:48:14 +0200

    Progress

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -8,9 +8,14 @@ module.exports = {
           '0%': { opacity: '0', transform: 'translateY(-20px)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
         },
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
       },
       animation: {
         fallIn: 'fallIn 0.5s ease forwards',
+         marquee: 'marquee 100s linear infinite',
       },
     },
   },
