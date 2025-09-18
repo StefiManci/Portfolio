ea95a3992e930d0739f0dbd49d4c76490613d4d9
Stefi <stefi.manci1@gmail.com>
2025-09-18 15:02:37 +0200

    Experience Progress

--- src/app/About/experience/experience.ts
+++ src/app/About/experience/experience.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
@@ -7,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrl: './experience.css'
 })
 export class Experience {
+ 
+   scrollProgress = 0;
 
+  @ViewChild('box3') box3!: ElementRef;
+
+  boxTop = 0;
+  boxHeight = 0;
+
+  ngAfterViewInit() {
+    // Get Box 3 position and height
+    const box = this.box3.nativeElement as HTMLElement;
+    this.boxTop = box.offsetTop;
+    this.boxHeight = box.offsetHeight;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.scrollY + window.innerHeight / 2; // adjust to center
+    if (scrollY < this.boxTop) {
+      this.scrollProgress = 0;
+    } else if (scrollY > this.boxTop + this.boxHeight) {
+      this.scrollProgress = 100;
+    } else {
+      this.scrollProgress = ((scrollY - this.boxTop) / this.boxHeight) * 100;
+    }
+  }
 }
