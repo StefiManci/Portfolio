68190bec4db69a4069bd2130a72c1d0845eee908
Stefi <stefi.manci1@gmail.com>
2025-10-24 13:21:03 +0200

    More Component Progresss....

--- src/app/More/more/more.ts
+++ src/app/More/more/more.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { Footer } from "../../Shared/footer/footer";
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartType } from 'chart.js';
 
 @Component({
   selector: 'app-more',
-  imports: [],
+  standalone: true,
+  imports: [Footer,BaseChartDirective],
   templateUrl: './more.html',
-  styleUrl: './more.css'
+  styleUrls: ['./more.css']
 })
-export class More {
+export class More { 
+journeyData: ChartConfiguration<'line'>['data'] = {
+    labels: ['2019', '2020', '2021', '2022', '2023', '2024','2025'],
+    datasets: [
+      {
+        label: 'Milestones',
+        data: [1, 3, 6, 9, 12, 15,12],
+        borderColor: 'rgba(37,99,235,0.9)',
+        backgroundColor: 'rgba(37,99,235,0.2)',
+        fill: true,
+        tension: 0.3,
+        pointRadius: 6,
+        pointHoverRadius: 10,
+      },
+    ],
+  };
 
+  // Chart options
+  journeyOptions: ChartConfiguration<'line'>['options'] = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: { display: false },
+      tooltip: { enabled: true },
+    },
+    scales: {
+      x: { title: { display: true, text: 'Year' } },
+      y: { title: { display: true, text: 'Achievements' } },
+    },
+  };
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { routes } from './app.routes';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,6 +13,7 @@ export const appConfig: ApplicationConfig = {
         scrollPositionRestoration: 'top', 
         anchorScrolling: 'enabled'       
       })
-    )
+    ),
+    provideCharts(withDefaultRegisterables())
   ]
 };
