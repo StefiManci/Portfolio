665c77229cd070f35b9064d5587b2576d70fbb3e
Stefi <stefi.manci1@gmail.com>
2025-10-27 10:53:55 +0100

    MVP Completed....

--- src/app/More/more/more.ts
+++ src/app/More/more/more.ts
@@ -40,4 +40,34 @@ journeyData: ChartConfiguration<'line'>['data'] = {
       y: { title: { display: true, text: 'Achievements' } },
     },
   };
+
+  // Ability
+  chosenAbility: string = '';
+  abilityDescription: string ="";
+
+  selectAbility(ability: string) {
+    this.chosenAbility = ability;
+
+    const descriptions: { [key: string]: string } = {
+    'E-Commerce Stores': 'Full-featured online stores with product management, cart functionality, secure checkout, and payment gateway integration. Designed for performance, user experience, and scalability.',
+
+    'Customer Portals / CRM System': 'Secure platforms for managing clients, communication, and data — including user profiles, support tickets, and analytics for business insights.',
+
+    'Analytics Dashboards': 'Interactive dashboards for tracking KPIs, visualizing data, and managing reports. Built with charting libraries and dynamic APIs for real-time analytics.',
+
+    'Authentication & User Management Systems': 'Secure login and registration systems using JWT or OAuth, complete with role-based access control, password recovery, and account verification.',
+
+    'RESTful & GraphQL APIs': 'Robust backends designed for performance and modularity — supporting CRUD operations, authentication, and data integration for frontend or mobile apps.',
+
+    'Microservices Architecture Projects': 'Scalable systems split into independent services communicating via APIs — enabling modular development, easier maintenance, and high reliability.',
+
+    'Banking & FinTech Platforms': 'Financial applications for transactions, balance management, fund transfers, and audit logs — with strict data validation and security compliance.',
+
+    'Saas Products': 'Subscription-based web applications offering business tools, dashboards, and automation. Includes authentication, billing, and scalable architecture.',
+
+    'Web-Applications': 'General-purpose dynamic web apps — from dashboards and task managers to productivity tools — focused on clean UI, responsive design, and solid backend logic.',
+  };
+
+  this.abilityDescription = descriptions[ability] || 'Select an ability to see details.';
+  }
 }
