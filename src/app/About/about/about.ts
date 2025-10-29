1707c249d0453899152a5cd14d4854d44eefcbaf
Stefi <stefi.manci1@gmail.com>
2025-10-29 14:29:49 +0100

    About => Education and Improvements

--- src/app/About/about/about.ts
+++ src/app/About/about/about.ts
@@ -3,10 +3,11 @@ import { SelfPresentation } from "../self-presentation/self-presentation";
 import { Experience } from "../experience/experience";
 import { Explore } from "../explore/explore";
 import { Footer } from "../../Shared/footer/footer";
+import { Education } from "../education/education";
 
 @Component({
   selector: 'app-about',
-  imports: [SelfPresentation, Experience, Explore, Footer],
+  imports: [SelfPresentation, Experience, Explore, Footer, Education],
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
